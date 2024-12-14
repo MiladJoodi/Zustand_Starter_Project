cdf31653b748739570433776163f88ab9578621e
Milad Joodi <winbeta.com@gmail.com>
2024-12-14 21:47:12 +0330

    Persist and Save to Local Storage ✔

--- src/store/counterStore.ts
+++ src/store/counterStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface ICounterStore {
   counter: number;
@@ -6,10 +7,18 @@ interface ICounterStore {
   decrement : ()=> void;
   reset : ()=> void;
 }
-export const useCounterStore = create<ICounterStore>((set) => ({
-  counter: 0,
-
-  increment: () => set(state => ({ counter: state.counter + 1 })),
-  decrement: () => set(state => ({ counter: state.counter - 1 })),
-  reset: () => set({ counter: 0 }),
-}));
+export const useCounterStore = create<ICounterStore>()(
+  persist(
+    (set) => ({
+      counter: 0,
+    
+      increment: () => set(state => ({ counter: state.counter + 1 })),
+      decrement: () => set(state => ({ counter: state.counter - 1 })),
+      reset: () => set({ counter: 0 }),
+    }),
+    {
+      name: "counter-store",
+      storage: createJSONStorage(()=> localStorage),
+    }
+  )
+);
